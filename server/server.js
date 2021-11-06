5e2dc3d58d5d9aec44baf3e804a81e373099dfad
Shubham Kapoor <shubham.kapoor01@gmail.com>
2021-11-06 19:56:42 +0530

    heroku added dist

--- server/server.js
+++ server/server.js
@@ -74,6 +74,7 @@ io.on('connection', socket => {
 
 if (process.env.NODE_ENV == "production") {
     app.use(express.static("client/build"));
+    app.use('/', express.static(path.join(__dirname, 'dist')));
 }
 
 const port = process.env.PORT || 3001;
