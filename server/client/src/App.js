821af12d8da72e57ac657b9f2a4fd7c95d4d9580
Shubham Kapoor <shubham.kapoor01@gmail.com>
2021-11-22 22:30:01 +0530

    auth

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import JoinRoom from "./JoinRoom";
 import Room from './Room';
 
-// TODO add mongodb login auth
+// TODO add mongodb login authentication
 
 function App() {
   return (
