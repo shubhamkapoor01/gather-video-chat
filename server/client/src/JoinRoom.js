b1c89d6302224b1c3fa44989a81826aabec9100e
Shubham Kapoor <shubham.kapoor01@gmail.com>
2022-03-14 22:30:30 +0530

    deploying

--- server/client/src/JoinRoom.js
+++ server/client/src/JoinRoom.js
@@ -4,7 +4,7 @@ import { v1 as uuid } from "uuid";
 import io from "socket.io-client";
 import e from "cors";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect(window.location.href);
 
 function JoinRoom(props) {
   const [name, setName] = useState("");
